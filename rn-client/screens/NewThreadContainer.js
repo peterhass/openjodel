fb75418e4d4f9aee37f4b8560e05253faa0291f5
Peter Haßler <pet.hassler@gmail.com>
2018-10-04 09:51:03 +0200

    Change back navigation: Remove new-thread screen from navigation stack

--- rn-client/screens/NewThreadContainer.js
+++ rn-client/screens/NewThreadContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import NewThread from './NewThread'
 import gql from 'graphql-tag'
 import { Query, Mutation } from 'react-apollo'
+import { StackActions, NavigationActions } from 'react-navigation'
 
 const CREATE_THREAD_MUTATION = gql`
 mutation CreateThread($message: String) {
@@ -19,14 +20,17 @@ mutation CreateThread($message: String) {
 class NewThreadContainer extends Component {
   static navigationOptions = NewThread.navigationOptions
   
+  constructor() {
+    super()
+    this.navigateToThread = this.navigateToThread.bind(this)
+  }
+  
   render() {
     return (
       <Mutation 
         mutation={CREATE_THREAD_MUTATION}
         refetchQueries={['GetThreads']}
-        onCompleted={({ createThread: thread }) => {
-          this.props.navigation.navigate('Thread', { id: thread.id })
-        }}
+        onCompleted={({ createThread: thread }) => this.navigateToThread({ thread })}
       >
         {mutation => (
           <NewThread
@@ -36,6 +40,20 @@ class NewThreadContainer extends Component {
         )}
       </Mutation>
     )
+  }
+
+  navigateToThread({ thread }) {
+    const setBackToThreadList = () => {
+      this.props.navigation.dispatch(StackActions.reset({
+        index: 0,
+        actions: [
+          NavigationActions.navigate({ routeName: 'Home' })
+        ]
+      }))
+    }
+
+    setBackToThreadList()
+    this.props.navigation.navigate('Thread', { id: thread.id })
   }
 }
 
